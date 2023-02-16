cda7e00260b01cbacee9672e17c408e09cd45ada
Andre Angelantoni <aangel@mac.com>
2023-02-16 15:01:08 -0700

    Update tests for defaultCommandTimeout.

--- cypress/e2e/performantlabs.com/contact-us/validate-contact-us.cy.js
+++ cypress/e2e/performantlabs.com/contact-us/validate-contact-us.cy.js
@@ -1,4 +1,5 @@
 /// <reference types="Cypress" />
+/// <reference types="cypress-data-session" />
 
 import { createRandomString } from '../../../support/utils.js'
 
@@ -7,20 +8,24 @@ describe('Validate Contact Us', {tags: ['contact-us', 'anonymous', 'smoke']}, ()
     cy.prepareForTestRun()
   })
 
-  it("(PER-1100) Contact Us form accepts correct input", {defaultCommandTimeout: 1000, languages: ['en'],  regions: ['us'], tags: ['contact-us', 'anonymous', 'smoke']}, () => {
+  it("(PER-1100) Contact Us form accepts correct input", {languages: ['en'],  regions: ['us'], tags: ['contact-us', 'anonymous', 'smoke']}, () => {
     const randomString = createRandomString(10)
-    cy.log("** Fill out contact form **")
+    cy.log("**Fill out contact form**")
     cy.visit('contact-us').then(() => {
       cy.get('#edit-name').type('André Angelantoni')
       cy.get('#edit-email').type('aangel100+001@gmail.com')
       cy.get('#edit-company-name').type('Performant Labs Inc.')
-      // We will check for this later.
+
+      cy.wrap(randomString).log("**Check for %o in the submitted form.**")
       cy.get('#edit-message').type(randomString)
       cy.contains('Send message').click()
     })
 
-    // Should see the thank-you page.
+    cy.log("**Should see the thank-you page.**")
     cy.contains('Thank you')
+    cy.visit(Cypress.env('url').login)
+    // cy.wrap(Cypress.env()).log('Environment variables %o')
+    // cy.wrap(Cypress.config()).log('Configuration variables %o')
 
     // But there shouldn't be an error on it.
     // cy.get('.alert').should('not.exist')
@@ -29,26 +34,21 @@ describe('Validate Contact Us', {tags: ['contact-us', 'anonymous', 'smoke']}, ()
     // Note that experiencing this problem and clearing isn't working:
     // https://github.com/cypress-io/cypress/issues/14590
     Cypress.session.clearSessionStorage
+
     cy.logInAs(Cypress.env('account').admin)
     cy.visit('admin/structure/webform/submissions/manage')
 
     // Check only some of them.
     var submissionIndex = 0
 
-    // TODO: Catching the error is failing.
-    // cy.on('fail', (e) => {
-    //   Absorb the error.
-    //   return false
-    // })
-
     cy.get('.webform-results-table > tbody > tr').each((row) => {
       cy.visit(row[0].dataset.webformHref)
-      cy.get('#contact--message').then( (element) => {
-        cy.wrap(element[0]).contains(randomString)
-      })
+      cy.wrap(randomString).log("**Now looking for %o in the submitted form.**")
+      cy.contains(randomString)
+
       submissionIndex++
       // If we checked all of them with no success, fail the test
-      if (submissionIndex = 10 ) return false  // Fail test.
+      if (submissionIndex == 1 ) return false  // Fail test.
     })
   })
 })
