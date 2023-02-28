9ef8882543034aae4a4ec96d7003876473d88718
Andre Angelantoni <aangel@mac.com>
2023-02-28 15:18:16 -0700

    Adjust config.

--- project/scripts/promote-admins.sh
+++ project/scripts/promote-admins.sh
@@ -1,35 +1,3 @@
 #!/bin/bash -x
 
-drush user:role:add administrator "\
-  ali.asalam.ur@bp.renesas.com, \
-  aluchsinger, \
-  amarlata.kumari.zj@renesas.com, \
-  andre.angelantoni.xk@renesas.com, \
-  ashok.kumar.kc@Renesas.com, \
-  bmtest, \
-  brandon.stone.xw@renesas.com, \
-  brendon.mosher.jy@renesas.com, \
-  bryans@imagexmedia.com, \
-  elly.millican.kf@bp.renesas.com, \
-  elvis@imagexmedia.com, \
-  emin.asif.gx@bp.renesas.com, \
-  guillermo@imagexmedia.com, \
-  igorg@imagexmedia.com, \
-  janez@tag1consulting.com, \
-  jody.hamilton.zj@renesas.com, \
-  joel.pineda.sx@renesas.com, \
-  juan.orrego.ak@bp.renesas.com, \
-  kelvin@imagexmedia.com, \
-  levi@imagexmedia.com, \
-  macarena-rocio.toloza.cp@bp.renesas.com, \
-  mark.aceves.vx@renesas.com, \
-  matthew.klein.uh@bp.renesas.com, \
-  nagarajan.kalaimani.xk@renesas.com, \
-  oleksandr.linyvyi@imagexmedia.com, \
-  randall.huamani.wh@bp.renesas.com, \
-  scott.spector.rh@renesas.com, \
-  sureshkumar.rajendran.xa@renesas.com, \
-  swati.sharma.zc@renesas.com, \
-  vasyl@imagexmedia.com, \
-  mateus.silva.vz@bp.renesas.com \
-  "
+#drush user:role:add administrator ""
