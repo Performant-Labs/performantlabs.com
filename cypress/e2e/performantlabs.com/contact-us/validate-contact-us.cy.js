85ef988bbf3837b50a6c4f96352bf54aeeebe067
Andre Angelantoni <aangel@mac.com>
2023-06-17 12:35:23 -0700

    Update cypress.

--- cypress/e2e/performantlabs.com/contact-us/validate-contact-us.cy.js
+++ cypress/e2e/performantlabs.com/contact-us/validate-contact-us.cy.js
@@ -2,7 +2,6 @@
 /// <reference types="cypress-data-session" />
 
 import { createRandomString } from '../../../support/utils.js'
-import 'cypress-recurse/commands'
 
 describe('Validate Contact Us', {tags: ['contact-us', 'anonymous', 'smoke']}, () => {
   before(function () {
@@ -11,7 +10,6 @@ describe('Validate Contact Us', {tags: ['contact-us', 'anonymous', 'smoke']}, ()
 
   it("(PER-1100) Contact Us form accepts correct input", {defaultCommandTimeout: 1000, languages: ['en'],  regions: ['us'], tags: ['contact-us', 'anonymous', 'smoke']}, () => {
     const randomString = createRandomString(10)
-
     cy.log("**Fill out contact form.**")
     cy.visit('contact-us').then(() => {
       cy.get('#edit-name').type('André Angelantoni')
@@ -20,13 +18,16 @@ describe('Validate Contact Us', {tags: ['contact-us', 'anonymous', 'smoke']}, ()
 
       // We will check for this later.
       cy.get('#edit-message').type(randomString)
-      cy.log("Looking for " + randomString)
       cy.contains('Send message').click()
     })
 
     cy.log("**Should see the thank-you page.**")
     cy.contains('Thank you')
     cy.visit(Cypress.env('url').login)
+    // cy.wrap(Cypress.env()).log('Environment variables %o')
+
+    // But there shouldn't be an error on it.
+    // cy.get('.alert').should('not.exist')
 
     // Now check for the entry in the database.
     // Note that experiencing this problem and clearing isn't working:
@@ -39,19 +40,21 @@ describe('Validate Contact Us', {tags: ['contact-us', 'anonymous', 'smoke']}, ()
     // Check only some of them.
     var submissionIndex = 0
 
-    // cy.log("**Look through table for entry.**")
-
-    // cy.get('.webform-results-table > tbody > tr').each((row) => {
-    //   cy.visit(row[0].dataset.webformHref)
-
-    //   cy.contains(randomString)
-
-    //   submissionIndex++
-    //   // If we checked all of them with no success, fail the test
-    //   if (submissionIndex == 1 ) {
-    //     return false  // Fail test.
-    //   }
-    //   return true
+    // TODO: Catching the error is failing.
+    // cy.on('fail', (e) => {
+    //   Absorb the error.
+    //   return false
     // })
+
+    cy.log("**Look through table for entry.**")
+    cy.get('.webform-results-table > tbody > tr').each((row) => {
+      cy.visit(row[0].dataset.webformHref)
+      cy.get('#contact--message').then( (element) => {
+        cy.wrap(element[0]).contains(randomString)
+      })
+      submissionIndex++
+      // If we checked all of them with no success, fail the test
+      if (submissionIndex == 10 ) return false  // Fail test.
+    })
   })
 })
