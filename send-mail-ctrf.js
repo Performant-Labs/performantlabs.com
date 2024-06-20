90683ba64ecba4974545fa8fab8dd17f4f053e46
Ilya Lyaukin <kzerby@gmail.com>
2024-06-20 09:51:14 -0600

    Add the subject of the email to GitHib variables; rename vars and secrets with ATK_ prefix; add a send email script locally and add test info to the email body.

---
+++ send-mail-ctrf.js
@@ -0,0 +1,56 @@
+// Parse report
+const report = require('./ctrf/ctrf-report.json');
+
+// Check that environment is set
+const missingVars =
+  ['MAIL_TLS_ENABLE', 'MAIL_HOST', 'MAIL_PORT', 'MAIL_USER', 'MAIL_PASSWORD',
+  'MAIL_FROM', 'MAIL_TO', 'MAIL_SUBJECT'].filter((key) => !process.env[key]);
+if (missingVars.length) {
+  console.error("Environment variables are missing: " + missingVars.join(', '));
+  process.exit(1);
+}
+
+// Use at least Nodemailer v4.1.0
+const nodemailer = require('nodemailer');
+
+// Create a SMTP transporter object
+let tlsEnable = process.env.MAIL_TLS_ENABLE === 'true';
+let transporter = nodemailer.createTransport({
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
+  secureConnection: !tlsEnable,  // TLS requires secureConnection to be false
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASSWORD
+  },
+  tls: !tlsEnable ? undefined : {
+    ciphers: 'SSLv3'
+  }
+});
+
+// Message object
+let subject = process.env.MAIL_SUBJECT.replaceAll(/\$\w+/g, match => process.env[match.substring(1)]);
+let message = {
+  from: process.env.MAIL_FROM,
+  to: process.env.MAIL_TO,
+  subject: subject,
+  text: `${report.results.summary.passed === report.results.summary.tests ?
+    'All tests passed' :
+    `${report.results.summary.failed} tests failed.`}\n
+    View workflow run: ${process.env.BUILD_URL}`,
+  html: `<p>${report.results.summary.passed === report.results.summary.tests ?
+    'All tests passed.' :
+    `<strong>${report.results.summary.failed}</strong> tests failed.`}</p>
+    <p><a href="${process.env.BUILD_URL}">View workflow run</a></p>`
+};
+
+transporter.sendMail(message, (err, info) => {
+  if (err) {
+    console.log('Error occurred. ' + err.message);
+    return process.exit(1);
+  }
+
+  console.log('Message sent: %s', info.messageId);
+  // Preview only available when sending through an Ethereal account
+  console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+});
