600f2ad45411021937d8d208c162d604aaa46e66
Ilya Lyaukin <kzerby@gmail.com>
2024-07-01 16:32:05 -0600

    Change visual tests to use locations from the file (or sitemap). Change snapshot structure.

--- playwright.config.js
+++ playwright.config.js
@@ -29,10 +29,11 @@ module.exports = defineConfig({
       buildUrl: process.env.BUILD_URL || 'BUILD_URL is not set',
     }]
   ],
+  snapshotPathTemplate: '{testDir}/{testFilePath}-snapshots/{arg}/{projectName}-{platform}.png',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
+    baseURL: 'https://performantlabs.com/',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
