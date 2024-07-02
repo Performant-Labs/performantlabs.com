668cae9f7f2323d336794bf17ae500308d6bed38
Ilya Lyaukin <kzerby@gmail.com>
2024-07-02 10:07:34 -0600

    Accessibility tests

---
+++ tests/atk_accessibility/atk_accessibility.spec.js
@@ -0,0 +1,23 @@
+const { test, expect } = require('@playwright/test');
+
+const atk_commands = require('../support/atk_commands');
+const AxeBuilder = require('@axe-core/playwright');
+
+test.describe('(ATK-PW-1600) Automatic detection of accessibility problems. @accessibility @ATK-PW-1600', async () => {
+  const locations = await atk_commands.getLocationsFromFile({ file: __filename });
+
+  for (let location of locations) {
+    test(location, async ({ page }, testInfo) => {
+      await page.goto(location);
+
+      const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+
+      await testInfo.attach('accessibility-scan-results', {
+        body: JSON.stringify(accessibilityScanResults, null, 2),
+        contentType: 'application/json'
+      });
+
+      expect(accessibilityScanResults.violations.length, 'number of problems').toEqual(0);
+    });
+  }
+});
