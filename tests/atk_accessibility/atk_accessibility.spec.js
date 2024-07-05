1ed523daaf6d659ece060b4f1cd5e573fa9ca630
Ilya Lyaukin <kzerby@gmail.com>
2024-07-05 12:28:01 -0600

    Add HTML report for Axe

--- tests/atk_accessibility/atk_accessibility.spec.js
+++ tests/atk_accessibility/atk_accessibility.spec.js
@@ -1,7 +1,11 @@
+import atkPlaywrightConfig from '../../playwright.atk.config';
+
 const { test, expect } = require('@playwright/test');
 
 const atk_commands = require('../support/atk_commands');
 const AxeBuilder = require('@axe-core/playwright');
+const { createHtmlReport } = require('axe-html-reporter');
+const fs = require('fs');
 
 let title = '(ATK-PW-1600) Automatic detection of accessibility problems. @accessibility @ATK-PW-1600';
 test.describe(title, async () => {
@@ -13,9 +17,18 @@ test.describe(title, async () => {
 
       const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
 
+      const htmlReport = createHtmlReport({
+        results: accessibilityScanResults,
+        options: {
+          doNotCreateReportFile: true
+        }
+      });
+
+      let htmlReportFile = `./axereport.html`;
+      fs.writeFileSync(htmlReportFile, htmlReport);
+
       await testInfo.attach('accessibility-scan-results', {
-        body: JSON.stringify(accessibilityScanResults, null, 2),
-        contentType: 'application/json'
+        path: htmlReportFile
       });
 
       expect(accessibilityScanResults.violations.length, 'number of problems').toEqual(0);
