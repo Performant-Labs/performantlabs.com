ac54e696d214fafb4193bae2d3060ba4ddcb5060
vikas <drupalchamp@gmail.com>
2025-05-15 11:14:24 +0000

    Uploaded js file related to new landing page

--- web/themes/custom/performant_labs/js/microsite.js
+++ web/themes/custom/performant_labs/js/microsite.js
@@ -1,24 +1,24 @@
 (function ($) {
-  $(document).ready(function () {
-    AOS.init();
+    $(document).ready(function () {
+        AOS.init();
 
-    var btn = $('#showVideo');
-    var video = $('#videoContainer');
-    var body = $('body'); // For detecting clicks outside the video
+        var btn = $('.show-intro-video');
+        var video = $('#videoContainer');
+        var body = $('body'); // For detecting clicks outside the video
 
-    if (btn.length && video.length) {
-      // Toggle video visibility on button click
-      btn.on('click', function () {
-        video.toggleClass('collapse');
-      });
+        if (btn.length && video.length) {
+            // Toggle video visibility on button click
+            btn.on('click', function () {
+                video.toggleClass('collapse');
+            });
 
-      // Close the video if clicked outside the video container
-      body.on('click', function (e) {
-        // If the click is outside the video container and the button
-        if (!video.is(e.target) && !btn.is(e.target) && video.has(e.target).length === 0 && btn.has(e.target).length === 0) {
-          video.addClass('collapse'); // Collapse the video
+            // Close the video if clicked outside the video container
+            body.on('click', function (e) {
+                // If the click is outside the video container and the button
+                if (!video.is(e.target) && !btn.is(e.target) && video.has(e.target).length === 0 && btn.has(e.target).length === 0) {
+                    video.addClass('collapse'); // Collapse the video
+                }
+            });
         }
-      });
-    }
-  });
+    });
 })(jQuery);
